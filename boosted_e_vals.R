96ff566780b78694cd482ccf943384ca34f8eaeb
fischer23 <88708145+fischer23@users.noreply.github.com>
2024-07-30 10:37:01 +0200

    Add files via upload

---
+++ boosted_e_vals.R
@@ -0,0 +1,79 @@
+#This file contains the functions for boosting the e-values used in the paper 
+#"An online generalization of the e-BH procedure"
+
+###Boosting with truncation function (6)
+##Input:
+#s:       Cutoff value for the truncation function, natural number.
+#alpha:   overall significance level, real number between 0 and 1.
+#gamma:   weighting sequence, n-dim. vector of real numbers between 0 and 1 with sum less than or equal to 1.
+#delta:   Parameter used for boosting the e-values, positive real number. In case of the simple null 
+#         H_i:X_i~N(mu_N,1) vs. alternative H_i^A:X_i~N(mu_A,1) delta should be set to delta=mu_A-mu_N.
+
+##Output: boosting factor.
+
+e_boosted_plus=function(s, alpha, gamma, delta){
+b_factor=function(b){
+    return(b*(1-pnorm(delta/2+log(s*alpha*gamma*b)/delta))+
+             sum((pnorm(delta/2-log((0:(s-1))*alpha*gamma*b)/delta)- pnorm(delta/2-log((1:s)*alpha*gamma*b)/delta))/((1:s)*alpha*gamma))-1)
+}
+return(uniroot(b_factor, lower=0, upper=10000)$root)
+}
+
+###Boosting with truncation function (7)
+##Input:
+#s:       cutoff value for the truncation function, natural number.
+#alpha:   overall significance level, real number between 0 and 1.
+#gamma:   weighting sequence, n-dim. vector of real numbers between 0 and 1 with sum less than or equal to 1.
+#delta:   Parameter used for boosting the e-values, positive real number. In case of the simple null 
+#         H_i:X_i~N(mu_N,1) vs. alternative H_i^A:X_i~N(mu_A,1) delta should be set to delta=mu_A-mu_N.
+
+##Output: boosting factor.
+
+e_boosted_minus=function(s, alpha, gamma, delta){
+  b_factor=function(b){
+    return(sum((pnorm(delta/2-log((0:(s-1))*alpha*gamma*b)/delta)- pnorm(delta/2-log((1:s)*alpha*gamma*b)/delta))/((1:s)*alpha*gamma))-1)
+  }
+  return(uniroot(b_factor, lower=0, upper=1000000)$root)
+}
+
+###Boosting with truncation function (8) with + adjustment
+##Input:
+#s:       cutoff value for the truncation function, natural number.
+#alpha:   overall significance level, real number between 0 and 1.
+#gamma:   weighting sequence, n-dim. vector of real numbers between 0 and 1 with sum less than or equal to 1.
+#delta:   Parameter used for boosting the e-values, positive real number. In case of the simple null 
+#         H_i:X_i~N(mu_N,1) vs. alternative H_i^A:X_i~N(mu_A,1) delta should be set to delta=mu_A-mu_N.
+#num_rej: number of previous (independent) rejections.
+
+##Output: boosting factor.
+
+e_boosted_plus_local=function(s, alpha, gamma, delta, num_rej){
+  k=num_rej+1
+  b_factor=function(b){
+    return(b*(1-pnorm(delta/2+log(s*alpha*gamma*b)/delta))+
+             sum((pnorm(delta/2-log((k:(s-1))*alpha*gamma*b)/delta)- pnorm(delta/2-log(((k+1):s)*alpha*gamma*b)/delta))/(((k+1):s)*alpha*gamma))+
+             (1- pnorm(delta/2-log(k*alpha*gamma*b)/delta))/(k*alpha*gamma)-1)
+  }
+  return(uniroot(b_factor, lower=0, upper=10000)$root)
+}
+
+###Boosting with truncation function (8) with - adjustment
+##Input:
+#s:       cutoff value for the truncation function, natural number.
+#alpha:   overall significance level, real number between 0 and 1.
+#gamma:   weighting sequence, n-dim. vector of real numbers between 0 and 1 with sum less than or equal to 1.
+#delta:   Parameter used for boosting the e-values, positive real number. In case of the simple null 
+#         H_i:X_i~N(mu_N,1) vs. alternative H_i^A:X_i~N(mu_A,1) delta should be set to delta=mu_A-mu_N.
+#num_rej: number of previous (independent) rejections.
+
+##Output: boosting factor.
+
+e_boosted_minus_local=function(s, alpha, gamma, delta, num_rej){
+  k=num_rej+1
+  b_factor=function(b){
+    return(sum((pnorm(delta/2-log((k:(s-1))*alpha*gamma*b)/delta)- pnorm(delta/2-log(((k+1):s)*alpha*gamma*b)/delta))/(((k+1):s)*alpha*gamma))+
+             (1- pnorm(delta/2-log(k*alpha*gamma*b)/delta))/(k*alpha*gamma)-1)
+  }
+  return(uniroot(b_factor, lower=0, upper=1000000)$root)
+}
+
---
+++ procedures.R
@@ -0,0 +1,96 @@
+#This file contains the procedures analyzed in the paper 
+#"An online generalization of the e-BH procedure"
+
+###online e-BH
+
+#Input:
+
+#alpha:   overall significance level, real number between 0 and 1.
+#gamma:   weighting sequence, n-dim. vector of real numbers between 0 and 1 with sum less than or equal to 1.
+#e:       e-values, n-dim. vector of nonnegative real numbers.
+#n:       number of hypotheses, natural number.
+
+#Output:
+
+#rejects: n-dim. vector of rejection indicators
+
+online_e_bh=function(alpha, gamma, e, n){
+  min_rej=n-rowSums((e>=(1/((alpha*gamma)%*%t((1:n))))))+1
+  k_star=max(c(which(colSums(outer(min_rej,(1:n), '<='))>=(1:n)),1))
+  rejects=(e>=(1/(alpha*gamma*k_star)))
+  return(rejects)
+} 
+
+###boosted online e-BH
+
+#Input:
+
+#alpha:   overall significance level, real number between 0 and 1.
+#gamma:   weighting sequence, n-dim. vector of real numbers between 0 and 1 with sum less than or equal to 1.
+#e:       e-values, n-dim. vector of nonnegative real numbers.
+#n:       number of hypotheses, natural number.
+#s:       maximum number of rejections, natural number.
+#delta:   mean difference between alternative and null hypothesis, nonnegative real number.
+
+#Output:
+
+#rejects: n-dim. vector of rejection indicators
+
+online_e_bh_boosted=function(alpha, gamma, e, n, s, delta){
+  for(i in 1:n){
+    e[i]=e[i]*e_boosted_minus(s, alpha, gamma[i], delta)
+  }
+  min_rej=n-rowSums((e>=(1/((alpha*gamma)%*%t((1:n))))))+1
+  k_star=max(c(which(colSums(outer(min_rej,(1:n), '<='))>=(1:n)),1))
+  rejects=(e>=(1/(alpha*gamma*k_star)))
+  return(rejects)
+} 
+
+###boosted online e-BH under local dependence
+
+#Input:
+
+#alpha:   overall significance level, real number between 0 and 1.
+#gamma:   weighting sequence, n-dim. vector of real numbers between 0 and 1 with sum less than or equal to 1.
+#e:       e-values, n-dim. vector of nonnegative real numbers.
+#n:       number of hypotheses, natural number.
+#s:       maximum number of rejections, natural number.
+#delta:   mean difference between alternative and null hypothesis, nonnegative real number.
+#lags:    lags that define the local dependence structure, n-dim. vector of natural numbers (including 0).
+
+#Output:
+
+#rejects: n-dim. vector of rejection indicators
+
+online_e_bh_boosted_local=function(alpha, gamma, e, n, s, delta, lags){
+  rejects=c()
+  for(i in 1:n){
+    e[i]=e[i]*e_boosted_minus_local(s, alpha, gamma[i], delta, sum(rejects[1:(i-lags[i]-1)]))
+    min_rej=i-rowSums((e[1:i]>=(1/((alpha*gamma[1:i])%*%t((1:i))))))+1
+    k_star=max(c(which(colSums(outer(min_rej,(1:i), '<='))>=(1:i)),1))
+    rejects=(e[1:i]>=(1/(alpha*gamma[1:i]*k_star)))
+  }
+  return(rejects)
+} 
+
+
+###e-lond
+
+#Input:
+
+#alpha:   overall significance level, real number between 0 and 1.
+#gamma:   weighting sequence, n-dim. vector of real numbers between 0 and 1 with sum less than or equal to 1.
+#e:       e-values, n-dim. vector of nonnegative real numbers.
+#n:       number of hypotheses, natural number.
+
+#Output:
+
+#rejects: n-dim. vector of rejection indicators
+
+e_lond=function(alpha, gamma, e, n){
+  rejects=rep(FALSE,n)
+  for(i in 1:n){
+    rejects[i]=(e[i]>= 1/(alpha * gamma[i] * (sum(rejects)+1)))
+  }
+  return(rejects)
+} 
